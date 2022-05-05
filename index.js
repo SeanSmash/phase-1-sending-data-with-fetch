60f1bf21e54698809b3040f2040236ffc344e913
Sean Sarokas <sps4462@gmail.com>
2022-05-05 15:33:59 -0400

    commit tree 154742e087a12ce90598baacdd3836d08aaccbbd

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+function submitData(userName, userEmail){
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        }),
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.id)
+        const newP = document.createElement('p')
+        newP.innerHTML = data.id
+        document.querySelector('body').appendChild(newP)
+    })
+    .catch(error => document.body.innerHTML = error.message)
+}
